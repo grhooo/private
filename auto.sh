69b95b714c76e5a73d1f1ea034a37aeb89d5749e
grhooo <62866580+grhooo@users.noreply.github.com>
2021-12-13 10:24:12 +0800

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -5,9 +5,7 @@ rm /etc/resolv.conf && echo -e 'nameserver 8.8.8.8\nnameserver 180.76.76.76' >> 
 timedatectl set-timezone Asia/Shanghai
 ## 创建自动更新sh文件，每天定时执行
 echo -e 'rm /usr/local/share/v2ray/*\nwget -P /usr/local/share/v2ray https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat' >> geodata.sh && chmod u+x geodata.sh && bash geodata.sh
-rm /var/spool/cron/crontabs/root && echo -e '30 6 * * * /root/geodata.sh' >> /var/spool/cron/crontabs/root
-## 关闭ufw记录
-ufw logging off && rm /var/log/ufw.log
+echo -e '30 6 * * * /root/geodata.sh' >> /var/spool/cron/crontabs/root
 ## 修改ssh端口
 if grep -q '^Port\s.*' /etc/ssh/sshd_config
 then
