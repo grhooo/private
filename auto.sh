74171a3e0041b376cae6ba7f4c188a7b381296c6
grhooo <62866580+grhooo@users.noreply.github.com>
2021-12-26 01:34:47 +0800

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,36 +1,56 @@
 #! /bin/sh
-echo -e "\n\033[32;1m【 修改DNS 】\033[0m"
+echo -e "\n\e[32;7m【 检查HOSTNAME 】\e[0m"
+echo $HOSTNAME | egrep -q '^[a-z]{2}\.[bopt]{4}\.[bopt]{3}$'
+if [ $? -eq 0 ]
+  then
+    echo -e ' \e[36;7m '$HOSTNAME' \e[0m'
+else
+  echo -e -n '\e[33;1m请输入hostname:\e[0m '
+  read input
+  echo -e ''$input'' | egrep -q '^[a-z]{2}\.[bopt]{4}\.[bopt]{3}$'
+  if [ $? -eq 0 ]
+    then
+      hostnamectl set-hostname $input
+      echo -e ' \e[36;7m '$HOSTNAME' \e[0m'
+  else
+    echo -e '\e[31;5m 输入有误，请重新运行本脚本！ \e[0m'
+    exit 1
+  fi
+fi
+echo -e "\n\e[32;7m【 修改DNS 】\e[0m"
 sed -i 's/domain-name-servers, //' /etc/dhcp/dhclient.conf
 echo -e 'nameserver 8.8.8.8\nnameserver 180.76.76.76' > /etc/resolv.conf
-echo -e "\n\033[32;1m【 修改ssh端口 】\033[0m"
+cat /etc/resolv.conf
+echo -e "\n\e[32;7m【 修改ssh端口 】\e[0m"
 if grep -q '^Port\s.*' /etc/ssh/sshd_config
 then
   sed -i 's/^Port\s.*/Port 27184/m' /etc/ssh/sshd_config
 else
   echo 'Port 27184' >> /etc/ssh/sshd_config
 fi
-echo -e "\n\033[32;1m【 安装caddy 】\033[0m"
+echo -en ' \e[36;7mSSH ' && cat /etc/ssh/sshd_config | grep '^Port\s.*' && echo -en '\e[0m'
+echo -e "\n\e[32;7m【 安装caddy 】\e[0m"
 wget -O /usr/bin/caddy https://caddyserver.com/api/download
 chmod 755 /usr/bin/caddy
 groupadd --system caddy
 wget -P /etc/systemd/system https://github.com/grhooo/private/raw/main/caddy.service
-echo -e "\033[32;1m【 安装xray 】\033[0m"
+echo -e "\e[32;7m【 安装xray 】\e[0m"
 wget -O /root/install.sh https://github.com/XTLS/Xray-install/raw/main/install-release.sh
 chmod u+x install.sh
 bash -c "$(cat /root/install.sh)" @ install --beta --without-geodata
 sed -i 's/User=nobody/User=root/g' /etc/systemd/system/xray.service
 sed -i 's/CapabilityBoundingSet=/#CapabilityBoundingSet=/g' /etc/systemd/system/xray.service
 sed -i 's/AmbientCapabilities=/#AmbientCapabilities=/g' /etc/systemd/system/xray.service
-echo -e "\n\033[32;1m【 每日4,16时检查更新 】\033[0m"
+echo -e "\n\e[32;7m【 每日4,16时检查更新 】\e[0m"
 timedatectl set-timezone Asia/Shanghai
 echo -e '0 4,16 * * * bash -c "$(cat /root/install.sh)" @ install --beta --without-geodata' > /var/spool/cron/crontabs/root
-echo -e "\033[33;1m【 crontab 】\033[0m"
+echo -e "\e[33;1m【 crontab 】\e[0m"
 crontab -l
-echo -e "\n\033[32;1m【 配置caddy/xray 】\033[0m"
+echo -e "\n\e[32;7m【 配置caddy/xray 】\e[0m"
 mkdir /etc/caddy
 mkdir /usr/share/caddy
 echo -e ':8080 {\n  root * /usr/share/caddy\n  file_server\n}\n'$HOSTNAME':80 {\n  redir https://'$HOSTNAME'{uri}\n}' > /etc/caddy/Caddyfile
-echo -e "\033[33;1m【 下载证书 】\033[0m"
+echo -e "\e[33;1m【 下载证书 】\e[0m"
 if echo $HOSTNAME | grep -q us
   then
     wget -P /usr/local/etc/xray https://github.com/grhooo/private/raw/main/cer/us/ptbt.crt https://github.com/grhooo/private/raw/main/cer/us/ptbt.key
@@ -48,22 +68,7 @@ systemctl daemon-reload
 systemctl enable --now caddy
 systemctl restart xray
 rm -rf /var/log/xray
-echo -e "\n\033[32;1m【 caddy/xray运行情况 】\033[0m"
+echo -e "\n\e[32;7m【 caddy/xray运行情况 】\e[0m"
 systemctl | grep -E 'caddy|xray' --color=auto
-echo -e "\n\033[32;1m【 /etc/caddy/Caddyfile 】\033[0m"
+echo -e "\n\e[32;7m【 /etc/caddy/Caddyfile 】\e[0m"
 cat /etc/caddy/Caddyfile
-echo -e -n "\n\033[32;1m 即将安装\033[0m \033[33;1mXanMod Kernel\033[0m [\033[31my\033[0mes|no]: "
-read input
-if [ "$input" = "yes" ] || [ "$input" = "y" ]
-  then
-    apt -y install gpg
-    echo deb http://deb.xanmod.org releases main > /etc/apt/sources.list.d/xanmod-kernel.list
-    curl -s https://dl.xanmod.org/gpg.key | gpg --no-default-keyring --keyring gnupg-ring:/etc/apt/trusted.gpg.d/xanmod-kernel.gpg --import
-    chown _apt /etc/apt/trusted.gpg.d/xanmod-kernel.gpg
-    apt update && apt -y install linux-xanmod
-    sed -i '/net.core.default_qdisc/d' /etc/sysctl.conf
-    echo net.core.default_qdisc=fq_pie >> /etc/sysctl.conf
-    sysctl --system
-  else
-    exit 0
-fi
