e53ca2b7c23d0d1197d34423041438559f30bd61
grhooo <62866580+grhooo@users.noreply.github.com>
2022-04-22 11:46:27 +0800

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -26,28 +26,21 @@ wget -t3 -O /usr/bin/caddy https://caddyserver.com/api/download
 chmod 755 /usr/bin/caddy
 groupadd --system caddy
 wget -t3 -P /etc/systemd/system https://github.com/grhooo/private/raw/main/caddy.service
-
-echo -e "\e[32;7m【 安装xray 】\e[0m"
-wget -t3 -O /root/install.sh https://github.com/XTLS/Xray-install/raw/main/install-release.sh
-chmod +x /root/*.sh
-bash -c "$(cat /root/install.sh)" @ install --beta --without-geodata
-sed -i -e 's/User=nobody/User=root/g' -e 's/CapabilityBoundingSet=/#CapabilityBoundingSet=/g' -e 's/AmbientCapabilities=/#AmbientCapabilities=/g' /etc/systemd/system/xray.service
-echo "net.core.rmem_max=2500000" >> /etc/sysctl.conf && sysctl -p
-
-echo -e "\n\e[32;7m【 设置定时任务 】\e[0m"
-timedatectl set-timezone Asia/Shanghai
-echo '0 4,16 * * * /bin/bash -c "$(cat /root/install.sh)" @ install --beta --without-geodata' > /var/spool/cron/crontabs/root
-echo '0 7 * * * /bin/wget -t3 -O /usr/share/caddy/geo.zip https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/rules.zip' >> /var/spool/cron/crontabs/root
-echo '30 */2 * * * /bin/wget -t3 -O /usr/share/caddy/rules.tar.gz https://github.com/grhooo/adfilters/releases/latest/download/rules.tar.gz && /bin/tar -xf /usr/share/caddy/rules.tar.gz -C /usr/share/caddy' >> /var/spool/cron/crontabs/root
-echo -e "\e[33;1m【 crontab 】\e[0m"
-crontab -l
-
-echo -e "\n\e[32;7m【 配置caddy/xray 】\e[0m"
 mkdir /etc/caddy
 mkdir /usr/share/caddy
+wget -t3 -P /usr/share/caddy https://github.com/grhooo/private/raw/main/index.html
 echo -e ':8080 {\n  root * /usr/share/caddy\n  file_server\n}\n'$HOSTNAME':80 {\n  redir https://'$HOSTNAME'{uri}\n}' > /etc/caddy/Caddyfile
 
-echo -e "\e[33;1m【 下载证书 】\e[0m"
+echo -e "\e[32;7m【 安装xray/hysteria 】\e[0m"
+wget -t3 -O /root/inst_x.sh https://github.com/XTLS/Xray-install/raw/main/install-release.sh
+wget -t3 -O /root/inst_h.sh https://raw.githubusercontent.com/HyNetwork/hysteria/master/install_server.sh
+chmod +x /root/*.sh
+bash -c "$(cat /root/inst_x.sh)" @ install --beta --without-geodata
+bash -c "$(cat /root/inst_h.sh)"
+sed -i -e 's/User=nobody/User=root/g' -e 's/CapabilityBoundingSet=/#CapabilityBoundingSet=/g' -e 's/AmbientCapabilities=/#AmbientCapabilities=/g' /etc/systemd/system/xray.service
+## echo "net.core.rmem_max=2500000" >> /etc/sysctl.conf && sysctl -p
+
+echo -e "\n\e[32;7m【 下载证书 】\e[0m"
 if echo $HOSTNAME | grep -q us
   then
     wget -t3 -P /usr/local/etc/xray https://github.com/grhooo/private/raw/main/cer/us/ptbt.{crt,key}
@@ -60,11 +53,13 @@ elif echo $HOSTNAME | grep -q hk
   else 
     echo -e '\e[31;5m 请手动下载证书至/usr/local/etc/xray！\e[0m'
 fi
-echo -e '{\n  "log": {\n    "access": "none",\n    "error": "none"\n  },\n  "inbounds": [\n    {\n      "port": 443,\n      "protocol": "vmess",\n      "settings": {\n        "clients": [\n          {\n            "id": "xray@ptbt.top"\n          }\n        ]\n      },\n      "streamSettings": {\n        "network": "quic",\n        "security": "tls",\n        "tlsSettings": {\n          "alpn": ["http/1.1"],\n          "certificates": [\n            {\n              "certificateFile": "/usr/local/etc/xray/ptbt.crt",\n              "keyFile": "/usr/local/etc/xray/ptbt.key"\n            }\n          ]\n        },\n        "quicSettings": {}\n      }\n    },\n    {\n      "port": 443,\n      "protocol": "vless",\n      "settings": {\n        "clients": [\n          {\n            "id": "xray@ptbt.top",\n            "flow": "xtls-rprx-direct"\n          }\n        ],\n        "decryption": "none",\n        "fallbacks": [\n          {\n            "dest": 8080\n          }\n        ]\n      },\n      "streamSettings": {\n        "network": "tcp",\n        "security": "xtls",\n        "xtlsSettings": {\n          "alpn": ["http/1.1"],\n          "certificates": [\n            {\n              "certificateFile": "/usr/local/etc/xray/ptbt.crt",\n              "keyFile": "/usr/local/etc/xray/ptbt.key"\n            }\n          ]\n        }\n      }\n    }\n  ],\n  "outbounds": [\n    {\n      "protocol": "freedom"\n    }\n  ]\n}' > /usr/local/etc/xray/config.json
+echo -e '{\n  "log": {\n    "access": "none",\n    "error": "none"\n  },\n  "inbounds": [\n    {\n      "port": 443,\n      "protocol": "vless",\n      "settings": {\n        "clients": [\n          {\n            "id": "xray@ptbt.top",\n            "flow": "xtls-rprx-direct"\n          }\n        ],\n        "decryption": "none",\n        "fallbacks": [\n          {\n            "dest": 8080\n          }\n        ]\n      },\n      "streamSettings": {\n        "network": "tcp",\n        "security": "xtls",\n        "xtlsSettings": {\n          "alpn": ["http/1.1"],\n          "certificates": [\n            {\n              "certificateFile": "/usr/local/etc/xray/ptbt.crt",\n              "keyFile": "/usr/local/etc/xray/ptbt.key"\n            }\n          ]\n        }\n      }\n    }\n  ],\n  "outbounds": [\n    {\n      "protocol": "freedom"\n    }\n  ]\n}' > /usr/local/etc/xray/config.json
+echo -e '{\n  "listen": ":33445",\n  "cert": "/usr/local/etc/xray/ptbt.crt",\n  "key": "/usr/local/etc/xray/ptbt.key",\n  "obfs": "66668888"\n}' > /etc/hysteria/config.json
 systemctl daemon-reload
 systemctl enable --now caddy
 systemctl restart xray
 rm -rf /var/log/xray
+systemctl enable hysteria-server && systemctl start hysteria-server
 
 echo -e "\n\e[32;7m【 安装dnsmasq 】\e[0m"
 apt -y install dnsmasq
@@ -81,8 +76,15 @@ else
 fi
 echo -en ' \e[36;7mSSH ' && cat /etc/ssh/sshd_config | grep '^Port\s.*' && echo -en '\e[0m'
 
-echo -e "\n\e[32;7m【 caddy/xray运行情况 】\e[0m"
-systemctl | grep -E 'caddy|xray' --color=auto
+echo -e "\n\e[32;7m【 设置定时任务 】\e[0m"
+timedatectl set-timezone Asia/Shanghai
+echo '0 4,16 * * * /bin/bash -c "$(cat /root/inst_x.sh)" @ install --beta --without-geodata' > /var/spool/cron/crontabs/root
+echo '0 5,17 * * * /bin/bash -c "$(cat /root/inst_h.sh)"' >> /var/spool/cron/crontabs/root
+echo '0 7 * * * /bin/wget -t3 -O /usr/share/caddy/geo.zip https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/rules.zip' >> /var/spool/cron/crontabs/root
+echo '30 */2 * * * /bin/wget -t3 -O /usr/share/caddy/rules.tar.gz https://github.com/grhooo/adfilters/releases/latest/download/rules.tar.gz && /bin/tar -xf /usr/share/caddy/rules.tar.gz -C /usr/share/caddy' >> /var/spool/cron/crontabs/root
+
+echo -e "\n\e[32;7m【 caddy/xray/hysteria运行情况 】\e[0m"
+systemctl | grep -E 'caddy|xray|hysteria' --color=auto
 echo -e "\n\e[32;7m【 /etc/caddy/Caddyfile 】\e[0m"
 cat /etc/caddy/Caddyfile
 echo -en "\n\e[41;5m  服务器必须重启！ \e[0m"
